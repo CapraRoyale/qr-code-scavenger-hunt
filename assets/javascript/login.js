3513f2f2fecc8c66f0743cea26ebb33d0b953d9f
Dan Gold <tentativechaos@gmail.com>
2019-08-05 20:25:20 -0700

    Finished logic for creating new user account. More error handling would be nice, but it's good enough as is. Added a authenticaiton state change listener that re-directs the user to a new page on a successfull login or new account creation.

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,62 +1,100 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyBrRR7AaTFxH1M2UpCLdFJErlKayHh3Vkw",
-    authDomain: "qr-code-hunt-for-red-october.firebaseapp.com",
-    databaseURL: "https://qr-code-hunt-for-red-october.firebaseio.com",
-    projectId: "qr-code-hunt-for-red-october",
-    storageBucket: "qr-code-hunt-for-red-october.appspot.com",
-    messagingSenderId: "912354708461",
-    appId: "1:912354708461:web:cf9e153278af2886"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// dbControll.js already initializes firebase and calls up the database
+// so we don't need to do any of that here. 
 
-// Assign firebase utilities to variables for easy access
-const database = firebase.database();
+// Handle for firebase authenticaiton
 const auth = firebase.auth();
 
 // jQuery hooks:
 const uNameField = $('#inputName');
 const uEmailField = $('#inputEmail');
 const uPassField = $('#InputPassword1');
-const errorDisplay = $('null');
+const errorName = $('#nameHelp');
+const errorEmail = $('#emailHelp');
+const errorPassword = $('#passwordHelp');
 
 const logInButton = $('#login-button');
 const newUserButton = $('#new-account-button');
 
+function invalidInput(field, response) {
+    // Function for handling specific types of invalid input
+    switch (field) {
+        case 'email':
+            uEmailField.addClass('is-invalid');
+            errorEmail.addClass('is-invalid');
+            errorEmail.text(response);
+            break;
+
+        case 'name':
+            uNameField.addClass('is-invalid');
+            errorName.addClass('is-invalid');
+            errorName.text(response);
+            break;
+
+        case 'password':
+            uPassField.addClass('is-invalid');
+            errorPassword.addClass('is-invalid');
+            errorPassword.text(response);
+            break;
+
+        default:
+            throw ('Input Error: "field" argument must be equal to "name", "email", or "password"');
+    }
+};
 
 // Create new account
 newUserButton.on("click", function (event) {
-    
-    // Don't refresh the page (for DEBUG Only)
+
+    // Don't refresh the page
     event.preventDefault()
 
     let uName = uNameField.val()
     let uPass = uPassField.val()
     let uEmail = uEmailField.val()
 
-    auth.createUserWithEmailAndPassword(uName, uPass).catch(function(error) {
-        switch(error.code) {
-            case 'auth/email-already-in-use' :
-                errorDisplay.text('That email address already belongs to another user!');
+    auth.createUserWithEmailAndPassword(uEmail, uPass).catch(function (error) {
+        console.log(error.message);
+        switch (error.code) {
+            // Bad email errors
+            case 'auth/email-already-in-use':
+                invalidInput('email', 'That email address already belongs to another user!');
                 break;
-            case 'auth/invalid-email': 
-                errorDisplay.text('Invalid email. Please enter a valid email.');
+            case 'auth/invalid-email':
+                invalidInput('email', 'Invalid email. Please enter a valid email.');
                 break;
+            // TODO: Add more handling 
+
+            // Bad password errors
+            // TODO: Add handling
+
+            // For all other errors just print the raw firebase error under the password field
+            default:
+                invalidInput('password', error.message);
         };
     });
 });
 
 logInButton.on("click", function (event) {
-    
+
     // Don't refresh the page (for DEBUG Only)
     event.preventDefault()
 
     let uName = uNameField.val()
     let uPass = uPassField.val()
 
-    auth.signInWithEmailAndPassword(uName, uPass).catch(function(error) {
+    auth.signInWithEmailAndPassword(uName, uPass).catch(function (error) {
         console.log(`Failed with error: ${error.code}`);
         console.log(error);
     });
+});
+
+// Authentication State Change Handler 
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        window.open("clue_page.html", "_self")
+    } else {
+        uNameField.val('');
+        uEmailField.val('');
+        uPassField.val('');
+    }
 });
