1ac6d6a123fe7e76a0919c43d8bf9245712dc785
Joshua Mannhalt <josh.mannhalt@gmail.com>
2019-08-08 18:16:19 -0700

    removed other classes/id .js file, updated list_of_id_classes.js with clue_page

--- assets/javascript/list_of_id_classes.js
+++ assets/javascript/list_of_id_classes.js
@@ -85,8 +85,32 @@ ________________________________________________________________________________
 (ids):
 
 
+/_________________________________________________Page 05: clue_page.html_____________________________________________________________________/
 
+// nav bar classes:
+// .navbar-toggler
+// .navbar-toggler-icon
+// .navbar navbar-expand-lg navbar-light bg-light
+// .navbar-brand
+// .collapse navbar-collapse
+// .navbar-nav mr-auto
+// .nav-item active
+// .nav-link
 
+// Inside jumbotron classes:
+// <jumbotron>
+// .display-4
+// .lead
+// .my-4
+// .row
+// .col-md-6 mb-4
+// .card card-cascade narrower
+// .view view-cascade gradient-card-header blue-gradient
+// .mb-0
+// .card-body card-body-cascade text-center
+// .map-container-google-8
+// .z-depth-1-half map-container-5
+// .btn btn-primary btn-lg
 
 
 
