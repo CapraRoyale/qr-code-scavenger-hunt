345ea8cc50d5b52c29f3a5cb3686f31696654c1d
Dan Gold <tentativechaos@gmail.com>
2019-08-08 20:24:11 -0700

    Creared file and added comments showing some initial methods I should make.

---
+++ assets/javascript/authTrack.js
@@ -0,0 +1,19 @@
+// Handle for firebase authenticaiton
+const auth = firebase.auth();
+
+const authentication = {    
+    
+    // Get user's unique firebase user ID
+    uID : function() {
+        //TODO
+    },
+
+    // Get user's display name
+
+
+    // Logout current user 
+
+
+    // Whenever auth-state changes, make sure the correct login/logout link is displayed on the nav-bar
+
+}
