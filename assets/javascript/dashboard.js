2a15252f0e6e54c066aac97ac5500fde68fdc455
Dan Gold <tentativechaos@gmail.com>
2019-08-11 15:57:35 -0700

    Finished writing the main code of the page that populates the table with rows of the user's games and buttons to interact with each.

--- assets/javascript/dashboard.js
+++ assets/javascript/dashboard.js
@@ -5,7 +5,7 @@ const mainDisplayArea = $('#game-display-area');
 // As soon as the page loads, we need to ping the database for all games owned by there user
 // Will return an array of objects that look like the following: {name: 'game name', id : 'directoryID'}
 // Because this is asynchronous, we need to deal with the output as a callback from this function
-dbi.getGames(function(gameList){
+dbi.getGames((gameList) => {
 
     // If user has no games, just print a message to that effect and exit the function
     if (gameList === null) {
@@ -15,13 +15,46 @@ dbi.getGames(function(gameList){
 
     // If user does have games, iterate through the list of games and add a table row and columns
     // Columns are 1) Game Name 2) Edit Button / Print Codes Button 3) Trck
-    else { 
+    else {
 
-        for (let i = 0; i < gameList.length; i++)
+        // Iterate through list of game objects
+        for (let i = 0; i < gameList.length; i++) {
 
-        let newRow = $('<tr>');
-        let nameCol = $('<td>').text(gameList[i].name);
-        let editCol = $('<button>').text('Edit/Print');
-        let trackCol = $('<button>').text('Track')
+            // Create new html elements
+            let newRow = $('<tr>');
+            let nameCol = $('<td>').text(gameList[i].name);
+
+            let editCol = $('<button>').text('Edit/Print');
+            editCol.addClass('.edit-link');
+
+            let trackCol = $('<button>').text('Track');
+            trackCol.addClass('.track-link')
+
+            // Assigin unique game IDs to buttons
+            editCol.attr('game', gameList[i].id)
+            trackCol.attr('game', gameList[i].id)
+
+            // Attach table cells to table row
+            newRow.append(nameCol);
+            newRow.append('<td>').append(editCol);
+            newRow.append('<td>').append(trackCol);
+
+            // Attach row to document inside existing 
+            mainDisplayArea.append(newRow);
+        }
+
+        // Set up event handlers for buttons
+        $('.edit-link').click(function () {
+
+            // When button is clicked, add game name to url as argument
+            window.location.href = `new_game.html?game=${$(this).attr('game')}`;
+        })
+
+        $('.track-link').click(function () {
+
+            // When button is clicked, add game name to url as argument
+            window.location.href = `tracking_page.html?game=${$(this).attr('game')}`;
+        })
+
     };
 });
